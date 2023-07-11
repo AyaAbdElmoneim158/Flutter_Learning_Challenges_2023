2b35254f9e57aea09eda97a1fb07070417ea0a36
Ayagithubweb <ayaabdelmon@gmail.com>
2023-07-11 10:38:09 +0300

    Add business_logic

--- app/lib/Bloc_session/business_logic/cubit/characters_cubit.dart
+++ app/lib/Bloc_session/business_logic/cubit/characters_cubit.dart
@@ -1,9 +1,26 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:app/Bloc_session/data/model/character_model.dart';
+import 'package:app/Bloc_session/data/repository/charachter_repository.dart';
 import 'package:bloc/bloc.dart';
-// ignore: depend_on_referenced_packages
-import 'package:meta/meta.dart';
+import 'package:flutter/material.dart';
 
 part 'characters_state.dart';
 
 class CharactersCubit extends Cubit<CharactersState> {
-  CharactersCubit() : super(CharactersInitial());
+  CharactersCubit({required this.charachterRepository})
+      : super(CharactersInitial());
+
+  CharachterRepository charachterRepository;
+
+  /// GetAllCharacters ---------------------------------------------------------
+  List<CharacterModel> charachters = [];
+  List<CharacterModel> getAllCharacters() {
+    charachterRepository.getAllCharacters().then((value) {
+      charachters = value;
+      emit(CharactersLoaded(charchters: value));
+    }).catchError((err) {
+      debugPrint("Err : to GetAllCharacters .... $err");
+    });
+    return charachters;
+  }
 }
--- app/lib/Bloc_session/router.dart
+++ app/lib/Bloc_session/router.dart
@@ -1,16 +1,25 @@
-// Route<dynamic>? Function(RouteSettings)? onGenerateRoute
-
+import 'package:app/Bloc_session/business_logic/cubit/characters_cubit.dart';
 import 'package:app/Bloc_session/constants/string.dart';
+import 'package:app/Bloc_session/data/repository/charachter_repository.dart';
+import 'package:app/Bloc_session/data/services/charachter_services.dart';
 import 'package:app/Bloc_session/presentation/screens/details_screen.dart';
 import 'package:app/Bloc_session/presentation/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppRoutes {
   static Route? onGenerateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
       case AppString.homeRoutes:
         return MaterialPageRoute(
-          builder: (context) => const HomeScreen(),
+          builder: (context) => BlocProvider(
+            create: (context) => CharactersCubit(
+              charachterRepository: CharachterRepository(
+                charachterServices: CharachterServices(),
+              ),//! Add to constructor...................
+            ),
+            child: const HomeScreen(),
+          ),
         );
 
       case AppString.detailsRoutes:
